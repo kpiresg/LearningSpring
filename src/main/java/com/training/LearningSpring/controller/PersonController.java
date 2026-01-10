6fcfd5b9f509c7063becd7a935af25adca64be20
KauanPires <kauanpiresgomes@gmail.com>
2026-01-10 19:45:55 -0300

    Implemented addPerson

--- src/main/java/com/training/LearningSpring/controller/PersonController.java
+++ src/main/java/com/training/LearningSpring/controller/PersonController.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +29,11 @@ public class PersonController {
   public ResponseEntity<List> listPerson() {
     List<Person> persons = personService.findAll();
     return new ResponseEntity<List>(persons, HttpStatus.OK);
+  }
+
+  @PostMapping("/addperson")
+  public ResponseEntity<Person> addPerson(@RequestBody Person person) {
+    personService.addPerson(person);
+    return new ResponseEntity(person, HttpStatus.CREATED);
   }
 }
